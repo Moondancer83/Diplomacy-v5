cd1fe25a6ec87711244887713157da3e7ba4c2a4
Mark Kalinovits <mkalinovits@chemaxon.com>
2017-12-03 14:19:07 +0100

    Implementing transport command check logics

--- logic/src/main/java/hu/kalee/diplomacy/order/TransportLogic.java
+++ logic/src/main/java/hu/kalee/diplomacy/order/TransportLogic.java
@@ -1,6 +1,8 @@
 package hu.kalee.diplomacy.order;
 
 import hu.kalee.diplomacy.domain.Command;
+import hu.kalee.diplomacy.domain.RegionType;
+import hu.kalee.diplomacy.domain.UnitType;
 
 /**
  * TransportLogic.
@@ -11,6 +13,31 @@ import hu.kalee.diplomacy.domain.Command;
 public class TransportLogic extends AbstractCommandLogic implements CommandLogic {
     @Override
     protected boolean specificChecks(final Command command) {
-        return false;
+        return precheck(command)
+                && isCapableOfTransporting(command)
+                && areBaseAndTargetReachable(command)
+                && isTargetValidForTransportee(command)
+                && isTargetUnoccupied(command);
+    }
+
+    private boolean precheck(final Command command) {
+        return command.getUnit() != null && command.getBase() != null && command.getTarget() != null;
+    }
+
+    private boolean isCapableOfTransporting(final Command command) {
+        return command.getUnit().getType().equals(UnitType.NAVY);
+    }
+
+    private boolean areBaseAndTargetReachable(final Command command) {
+        return command.getUnit().getLocation().getAdjacent().contains(command.getBase());
+    }
+
+    private boolean isTargetValidForTransportee(final Command command) {
+        return command.getBase().getUnit().getType().equals(UnitType.ARMY) && !command.getTarget().getType().equals(RegionType.SEA)
+                || command.getBase().getUnit().getType().equals(UnitType.NAVY) && !command.getTarget().getType().equals(RegionType.LAND);
+    }
+
+    private boolean isTargetUnoccupied(final Command command) {
+        return command.getTarget().getUnit() == null;
     }
 }
--- logic/src/test/java/hu/kalee/diplomacy/order/TransportLogicTest.java
+++ logic/src/test/java/hu/kalee/diplomacy/order/TransportLogicTest.java
@@ -4,6 +4,12 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import hu.kalee.diplomacy.domain.Command;
+import hu.kalee.diplomacy.domain.CommandType;
+import hu.kalee.diplomacy.domain.Region;
+import hu.kalee.diplomacy.domain.RegionType;
+import hu.kalee.diplomacy.domain.Unit;
+import hu.kalee.diplomacy.domain.UnitType;
 import static org.junit.Assert.*;
 
 /**
@@ -23,5 +29,40 @@ public class TransportLogicTest {
     @Test
     public void testCheckShouldFailWhenNoCommandIsGiven() {
         Assert.assertFalse(underTest.check(null));
+    }
+
+    @Test
+    public void testCheckShouldReturnTrueWhen() {
+        // GIVEN
+        final Region coast = new Region();
+        coast.setType(RegionType.COASTAL);
+        final Region base = new Region();
+        base.setType(RegionType.COASTAL);
+        final Region target = new Region();
+        target.setType(RegionType.COASTAL);
+
+        coast.getAdjacent().add(base);
+        coast.getAdjacent().add(target);
+
+        Unit transporter = new Unit();
+        transporter.setType(UnitType.NAVY);
+        transporter.setLocation(coast);
+
+        Unit transportee = new Unit();
+        transportee.setType(UnitType.ARMY);
+        transportee.setLocation(base);
+        base.setUnit(transportee);
+
+        Command transportCommand = new Command();
+        transportCommand.setType(CommandType.TRANSPORT);
+        transportCommand.setUnit(transporter);
+        transportCommand.setBase(base);
+        transportCommand.setTarget(target);
+
+        // WHEN
+        boolean actual = underTest.check(transportCommand);
+
+        // THEN
+        Assert.assertTrue(actual);
     }
 }
